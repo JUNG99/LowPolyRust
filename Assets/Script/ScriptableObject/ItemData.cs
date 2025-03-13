c387be86b1d36461b59f221a26d587e30ed87572
h0ldtighttt <h0ldtighttt@gmail.com>
2025-03-13 16:54:09 +0900

    feat : ItemObject.cs
    add ItemAssets - 2Dicons, Polygon-Lite Survival Collection
    add Item_Apple
    add Item_log

---
+++ Assets/Script/ScriptableObject/ItemData.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    food,
+    Resource,
+}
+
+public enum ConsumableType
+{
+    Hunger,
+    Water
+}
+
+[Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+
+
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
+}
