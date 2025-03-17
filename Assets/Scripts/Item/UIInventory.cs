b0553cfd25611016b9e5634f5bc409a514c16cc1
h0ldtighttt <h0ldtighttt@gmail.com>
2025-03-17 18:07:12 +0900

    fix : ItemSlot

--- Assets/Scripts/Item/UIInventory.cs
+++ Assets/Scripts/Item/UIInventory.cs
@@ -1,7 +1,5 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.InputSystem;
 
 public class UIInventory : MonoBehaviour
 {
@@ -9,35 +7,24 @@ public class UIInventory : MonoBehaviour
 
     public GameObject inventoryWindow;
     public Transform slotPanel;
-    public Transform dropPosition;
 
     [Header("Selected Item")]
-    private ItemSlot selectedItem;
-    private int selectedItemIndex;
     public TextMeshProUGUI selectedItemName;
     public TextMeshProUGUI selectedItemDescription;
-    public TextMeshProUGUI selectedItemStatName;
-    public TextMeshProUGUI selectedItemStatValue;
-    public GameObject useButton;
-    public GameObject equipButton;
-    public GameObject unEquipButton;
-    public GameObject dropButton;
 
-    private int curEquipIndex;
+    public GameObject DiscardButton;
 
     private PlayerController controller;
     private PlayerStats stats;
 
     void Start()
     {
-        controller = CharacterManager.Instance.Player.controller;
-        stats = CharacterManager.Instance.Player.stats;
-        dropPosition = CharacterManager.Instance.Player.dropPosition;
-
-        controller.inventory += Toggle;
-        CharacterManager.Instance.Player.addItem += AddItem;
+        
+        // controller = CharacterManager.Instance.Player.controller;
+        // stats = CharacterManager.Instance.Player.stats;
 
         inventoryWindow.SetActive(false);
+
         slots = new ItemSlot[slotPanel.childCount];
 
         for (int i = 0; i < slots.Length; i++)
@@ -45,193 +32,189 @@ public class UIInventory : MonoBehaviour
             slots[i] = slotPanel.GetChild(i).GetComponent<ItemSlot>();
             slots[i].index = i;
             slots[i].inventory = this;
-            slots[i].Clear();
         }
 
         ClearSelectedItemWindow();
     }
 
+    void Update()
+    {
+        
+    }
+
     void ClearSelectedItemWindow()
     {
-        selectedItem = null;
+        //selectedItem = null;
 
         selectedItemName.text = string.Empty;
         selectedItemDescription.text = string.Empty;
-        selectedItemStatName.text = string.Empty;
-        selectedItemStatValue.text = string.Empty;
-
-        useButton.SetActive(false);
-        equipButton.SetActive(false);
-        unEquipButton.SetActive(false);
-        dropButton.SetActive(false);
+       
+        DiscardButton.SetActive(false);
     }
 
-    public void Toggle()
-    {
-        if (IsOpen())
-        {
-            inventoryWindow.SetActive(false);
-        }
-        else
-        {
-            inventoryWindow.SetActive(true);
-        }
-    }
+    //public void Toggle()
+    //{
+    //    if (IsOpen())
+    //    {
+    //        inventoryWindow.SetActive(false);
+    //    }
+    //    else
+    //    {
+    //        inventoryWindow.SetActive(true);
+    //    }
+    //}
 
-    public bool IsOpen()
-    {
-        return inventoryWindow.activeInHierarchy;
-    }
+    //public bool IsOpen()
+    //{
+    //    return inventoryWindow.activeInHierarchy;
+    //}
 
-    // PlayerController 먼저 수정
+    //PlayerController 먼저 수정
 
-    public void AddItem()
-    {
-        ItemData data = CharacterManager.Instance.Player.itemData;
+    //public void AddItem()
+    //{
+    //    ItemData data = CharacterManager.Instance.Player.itemData;
 
-        if (data.canStack)
-        {
-            ItemSlot slot = GetItemStack(data);
-            if (slot != null)
-            {
-                slot.quantity++;
-                UpdateUI();
-                CharacterManager.Instance.Player.itemData = null;
-                return;
-            }
-        }
+    //    if (data.canStack)
+    //    {
+    //        ItemSlot slot = GetItemStack(data);
+    //        if (slot != null)
+    //        {
+    //            slot.quantity++;
+    //            UpdateUI();
+    //            CharacterManager.Instance.Player.itemData = null;
+    //            return;
+    //        }
+    //    }
 
-        ItemSlot emptySlot = GetEmptySlot();
+    //    ItemSlot emptySlot = GetEmptySlot();
 
-        if (emptySlot != null)
-        {
-            emptySlot.item = data;
-            emptySlot.quantity = 1;
-            UpdateUI();
-            CharacterManager.Instance.Player.itemData = null;
-            return;
-        }
+    //    if (emptySlot != null)
+    //    {
+    //        emptySlot.item = data;
+    //        emptySlot.quantity = 1;
+    //        UpdateUI();
+    //        CharacterManager.Instance.Player.itemData = null;
+    //        return;
+    //    }
 
-        ThrowItem(data);
-        CharacterManager.Instance.Player.itemData = null;
-    }
+    //    ThrowItem(data);
+    //    CharacterManager.Instance.Player.itemData = null;
+    //}
 
-    public void UpdateUI()
-    {
-        for (int i = 0; i < slots.Length; i++)
-        {
-            if (slots[i].item != null)
-            {
-                slots[i].Set();
-            }
-            else
-            {
-                slots[i].Clear();
-            }
-        }
-    }
+    //public void UpdateUI()
+    //{
+    //    for (int i = 0; i < slots.Length; i++)
+    //    {
+    //        if (slots[i].item != null)
+    //        {
+    //            slots[i].Set();
+    //        }
+    //        else
+    //        {
+    //            slots[i].Clear();
+    //        }
+    //    }
+    //}
 
-    ItemSlot GetItemStack(ItemData data)
-    {
-        for (int i = 0; i < slots.Length; i++)
-        {
-            if (slots[i].item == data && slots[i].quantity < data.maxStackAmount)
-            {
-                return slots[i];
-            }
-        }
-        return null;
-    }
+    //ItemSlot GetItemStack(ItemData data)
+    //{
+    //    for (int i = 0; i < slots.Length; i++)
+    //    {
+    //        if (slots[i].item == data && slots[i].quantity < data.maxStackAmount)
+    //        {
+    //            return slots[i];
+    //        }
+    //    }
+    //    return null;
+    //}
 
-    ItemSlot GetEmptySlot()
-    {
-        for (int i = 0; i < slots.Length; i++)
-        {
-            if (slots[i].item == null)
-            {
-                return slots[i];
-            }
-        }
-        return null;
-    }
+    //ItemSlot GetEmptySlot()
+    //{
+    //    for (int i = 0; i < slots.Length; i++)
+    //    {
+    //        if (slots[i].item == null)
+    //        {
+    //            return slots[i];
+    //        }
+    //    }
+    //    return null;
+    //}
 
-    // Player 스크립트 먼저 수정
-    public void ThrowItem(ItemData data)
-    {
-        Instantiate(data.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360));
-    }
+    //Player 스크립트 먼저 수정
+    //public void ThrowItem(ItemData data)
+    //{
+    //    Instantiate(data.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360));
+    //}
 
 
-    // ItemSlot 스크립트 먼저 수정
-    public void SelectItem(int index)
-    {
-        if (slots[index].item == null) return;
+    //ItemSlot 스크립트 먼저 수정
+    //public void SelectItem(int index)
+    //{
+    //    if (slots[index].item == null) return;
 
-        selectedItem = slots[index];
-        selectedItemIndex = index;
+    //    selectedItem = slots[index];
+    //    selectedItemIndex = index;
 
-        selectedItemName.text = selectedItem.item.displayName;
-        selectedItemDescription.text = selectedItem.item.description;
+    //    selectedItemName.text = selectedItem.item.displayName;
+    //    selectedItemDescription.text = selectedItem.item.description;
 
-        selectedItemStatName.text = string.Empty;
-        selectedItemStatValue.text = string.Empty;
+    //    selectedItemStatName.text = string.Empty;
+    //    selectedItemStatValue.text = string.Empty;
 
-        for (int i = 0; i < selectedItem.item.consumables.Length; i++)
-        {
-            selectedItemStatName.text += selectedItem.item.consumables[i].type.ToString() + "\n";
-            selectedItemStatValue.text += selectedItem.item.consumables[i].value.ToString() + "\n";
-        }
+    //    for (int i = 0; i < selectedItem.item.consumables.Length; i++)
+    //    {
+    //        selectedItemStatName.text += selectedItem.item.consumables[i].type.ToString() + "\n";
+    //        selectedItemStatValue.text += selectedItem.item.consumables[i].value.ToString() + "\n";
+    //    }
 
-        useButton.SetActive(selectedItem.item.type == ItemType.Consumable);
-        equipButton.SetActive(selectedItem.item.type == ItemType.Equipable && !slots[index].equipped);
-        unEquipButton.SetActive(selectedItem.item.type == ItemType.Equipable && slots[index].equipped);
-        dropButton.SetActive(true);
-    }
+    //    useButton.SetActive(selectedItem.item.type == ItemType.Consumable);
+    //    equipButton.SetActive(selectedItem.item.type == ItemType.Equipable && !slots[index].equipped);
+    //    unEquipButton.SetActive(selectedItem.item.type == ItemType.Equipable && slots[index].equipped);
+    //    dropButton.SetActive(true);
+    //}
 
-    public void OnUseButton()
-    {
-        if (selectedItem.item.type == ItemType.Consumable)
-        {
-            for (int i = 0; i < selectedItem.item.consumables.Length; i++)
-            {
-                switch (selectedItem.item.consumables[i].type)
-                {
-                    case ConsumableType.Health:
-                        stats.Heal(selectedItem.item.consumables[i].value); break;
-                    case ConsumableType.Hunger:
-                        stats.Eat(selectedItem.item.consumables[i].value); break;
-                }
-            }
-            RemoveSelctedItem();
-        }
-    }
+    //public void OnUseButton()
+    //{
+    //    if (selectedItem.item.type == ItemType.Consumable)
+    //    {
+    //        for (int i = 0; i < selectedItem.item.consumables.Length; i++)
+    //        {
+    //            switch (selectedItem.item.consumables[i].type)
+    //            {
 
-    public void OnDropButton()
-    {
-        ThrowItem(selectedItem.item);
-        RemoveSelctedItem();
-    }
+    //            }
+    //        }
+    //        RemoveSelctedItem();
+    //    }
+    //}
 
-    void RemoveSelctedItem()
-    {
-        selectedItem.quantity--;
+    //public void OnDropButton()
+    //{
+    //    ThrowItem(selectedItem.item);
+    //    RemoveSelctedItem();
+    //}
 
-        if (selectedItem.quantity <= 0)
-        {
-            if (slots[selectedItemIndex].equipped)
-            {
-                UnEquip(selectedItemIndex);
-            }
+    //void RemoveSelctedItem()
+    //{
+    //    selectedItem.quantity--;
 
-            selectedItem.item = null;
-            ClearSelectedItemWindow();
-        }
+    //    if (selectedItem.quantity <= 0)
+    //    {
+    //        if (slots[selectedItemIndex].equipped)
+    //        {
+    //            UnEquip(selectedItemIndex);
+    //        }
 
-        UpdateUI();
-    }
+    //        selectedItem.item = null;
+    //        ClearSelectedItemWindow();
+    //    }
 
-    public bool HasItem(ItemData item, int quantity)
-    {
-        return false;
-    }
+    //    UpdateUI();
+    //}
+
+    //public bool HasItem(ItemData item, int quantity)
+    //{
+    //    return false;
+    //}
 }
